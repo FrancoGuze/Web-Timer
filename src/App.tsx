f2664c84770d19e9b7d75e0c65ef882712a0109e
Franco Guze <franguze13@gmail.com>
2025-03-14 12:23:21 -0300

    Corrected time stopping problem while in another page

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,16 @@ function App() {
   const [running, setRunning] = useState(false);
   const intervalRef = useRef<number | null>(null);
 
+
   useEffect(() => {
     if (running) {
       intervalRef.current = setInterval(() => {
-        swap ? setTiempoEstudio((prev:number) => prev + 1) : setTiempoOtros((prev:number) => prev + 1);
+        const start = JSON.parse(localStorage.getItem('INICIO') || '').startingTime
+        setTiempoEstudio(() => swap ? Math.max(0, Math.floor((Date.now() - start) / 1000) - tiempoOtros) : tiempoEstudio);
+setTiempoOtros(() => !swap ? Math.max(0, Math.floor((Date.now() - start) / 1000) - tiempoEstudio) : tiempoOtros);
       }, 1000);
+
+
     } else if (intervalRef.current) {
       clearInterval(intervalRef.current);
     }
@@ -33,13 +38,35 @@ function App() {
   useEffect(() => {
     const tiempos = { estudio: tiempoEstudio, otros: tiempoOtros };
     localStorage.setItem('TIEMPOS', JSON.stringify(tiempos));
-  }, [tiempoEstudio, tiempoOtros]);
 
-  const changeSwap = () => setSwap(!swap);
+  }, [tiempoEstudio, tiempoOtros]);
+  const changeSwap = () => (
+    setSwap(!swap)
+  );
   const handleRestart = () => {
     setTiempoEstudio(0);
     setTiempoOtros(0);
     setRunning(false);
+    localStorage.clear()
+  };
+  const handlePause = () => {
+    if (running) {
+      localStorage.setItem('PAUSA', JSON.stringify({ pauseTime: Date.now() }));
+      setRunning(false);
+    } else {
+      const pausaGuardada = JSON.parse(localStorage.getItem('PAUSA') || '{}').pauseTime;
+
+      if (pausaGuardada) {
+        const tiempoPausado = Date.now() - pausaGuardada;
+
+        const inicioGuardado = JSON.parse(localStorage.getItem('INICIO') || '{}');
+        if (inicioGuardado.startingTime) {
+          localStorage.setItem('INICIO', JSON.stringify({ startingTime: inicioGuardado.startingTime + tiempoPausado }));
+        }
+      }
+
+      setRunning(true);
+    }
   };
 
   const calcHours = (val: number) => Math.floor((val / 60) / 60);
@@ -55,18 +82,18 @@ function App() {
       <div className="study area">
         <h1>Estudio</h1>
         <h2>{calcHours(tiempoEstudio)}:{formatTime(calcMinutes(tiempoEstudio))}:{formatTime(calcSeconds(tiempoEstudio))}</h2>
-        <button 
-          style={tiempoEstudio !== 0 || tiempoOtros !== 0 ? { display: 'none' } : { display: 'block' }} 
-          onClick={() => { setSwap(true); setRunning(true); }}>
+        <button
+          style={tiempoEstudio !== 0 || tiempoOtros !== 0 ? { display: 'none' } : { display: 'block' }}
+          onClick={() => { setSwap(true); setRunning(true); localStorage.setItem('INICIO', JSON.stringify({ startingTime: Date.now() })) }}>
           Start
         </button>
       </div>
       <div className="other area">
         <h1>Otros</h1>
         <h2>{calcHours(tiempoOtros)}:{formatTime(calcMinutes(tiempoOtros))}:{formatTime(calcSeconds(tiempoOtros))}</h2>
-        <button 
-          style={tiempoEstudio !== 0 || tiempoOtros !== 0 ? { display: 'none' } : { display: 'block' }} 
-          onClick={() => { setSwap(false); setRunning(true); }}>
+        <button
+          style={tiempoEstudio !== 0 || tiempoOtros !== 0 ? { display: 'none' } : { display: 'block' }}
+          onClick={() => { setSwap(false); setRunning(true); localStorage.setItem('INICIO', JSON.stringify({ startingTime: Date.now() })) }}>
           Start
         </button>
       </div>
@@ -76,13 +103,13 @@ function App() {
       </div>
 
       <div className={`buttons ${show}`}>
-        {tiempoEstudio === 0 && tiempoOtros === 0 
-          ? <button style={{ visibility: 'hidden' }}>asas</button> 
+        {tiempoEstudio === 0 && tiempoOtros === 0
+          ? <button style={{ visibility: 'hidden' }}>asas</button>
           : <button style={swap ? { backgroundColor: '#502d14' } : { backgroundColor: '#111846' }} onClick={changeSwap}>Swap</button>
         }
-        {running 
-          ? <button onClick={() => setRunning(false)}>Pausar</button> 
-          : <button onClick={() => setRunning(true)}>Continuar</button>
+        {running
+          ? <button onClick={() => handlePause()}>Pausar</button>
+          : <button onClick={() => handlePause()}>Continuar</button>
         }
         <button onClick={handleRestart}>Reiniciar</button>
       </div>
